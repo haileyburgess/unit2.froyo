525da8f549e40b552c81c40a740b2cac7030b7c1
Hailey Burgess <hailey.burgess@redcross.org>
2025-03-24 21:03:25 -0400

    added user input prompt

---
+++ script.js
@@ -0,0 +1,5 @@
+// prompt user to enter froyo flavors in a comma separated list format //
+const userInputString = prompt(
+    "Please enter fro-yo flavors you want to order as a list. We will tell you how many of each flavor you have ordered.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  );
