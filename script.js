80820212d4a66790fb33585dd772cc96a2421dec
Hailey Burgess <hailey.burgess@redcross.org>
2025-03-24 21:25:35 -0400

    split string into array of strings

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const userInputString = prompt(
     "Please enter fro-yo flavors you want to order as a list. We will tell you how many of each flavor you have ordered.",
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
   );
+
+  // split the string of numbers into an array of strings and log the result 
+const stringArray = userInputString.split(",");
+console.log(stringArray);
+
+// build an object to track flavors
